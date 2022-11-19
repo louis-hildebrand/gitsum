d216d6d6c719bb04676a9c1d8ef3b8fde3ce631b
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-11-19 00:39:46 -0500

    Rewrite script using pygit2

--- gitsum.py
+++ gitsum.py
@@ -1,109 +1,81 @@
-from subprocess import PIPE
-from typing import List, Union
+from pygit2 import Repository
+from typing import TypeVar
 import os
-import subprocess
+import pygit2
+
+
+T = TypeVar("T")
 
 
 class RepoStatus:
-    BRANCH_WIDTH = 10
+    def __init__(self, name: str, branch: str, is_local: bool, has_changes: bool, branch_has_upstream: bool, local_ahead: int, local_behind: int):
+        self._name = name
+        self._branch = branch
+        self._has_changes = has_changes
+        self._is_local = is_local
+        self._branch_has_upstream = branch_has_upstream
+        self._local_ahead = local_ahead
+        self._local_behind = local_behind
 
-    def __init__(self, full_path: str, name: str, branch: str, has_changes: bool, has_remote: bool, local_commits: Union[int, None], remote_commits: Union[int, None]):
-        self.full_path = full_path
-        self.name = name
-        self.branch = branch
-        self.has_changes = has_changes
-        self.has_remote = has_remote
-        self.local_commits = local_commits
-        self.remote_commits = remote_commits
-
-    def to_string(self, name_width: int) -> str:
-        is_up_to_date = not self.has_changes and (not self.has_remote or (self.local_commits == 0 and self.remote_commits == 0))
-        return f"{' ' if is_up_to_date else '!'}  {self.name:<{name_width}}  {self.branch:<{RepoStatus.BRANCH_WIDTH}} {' *' if self.has_changes else '  '}{f' >{self.local_commits}' if self.local_commits else '   '}{f' <{self.remote_commits}' if self.remote_commits else '   '}"
+    def to_string(self, name_width: int, head_width: int) -> str:
+        is_up_to_date = not self._has_changes
+        return f"{' ' if is_up_to_date else '!'}  {self._name:<{name_width}}  {'[LR]' if self._is_local else '[LB]' if not self._branch_has_upstream else '    '}  {self._branch:<{head_width}} {' *' if self._has_changes else '  '}{f' >{self._local_ahead}' if self._local_ahead > 0 else '   '}{f' <{self._local_behind}' if self._local_behind > 0 else '   '}"
 
 
-def _flatten(l: List[List[str]]) -> List[str]:
+def _flatten(l: list[list[T]]) -> list[T]:
     return [x for sublist in l for x in sublist]
 
 
-def _invoke_git(args: List[str], full_path: str) -> str:
-    result = subprocess.run(["git", "--git-dir", os.path.join(full_path, ".git"), "--work-tree", full_path] + args, stdout=PIPE)
-    return result.stdout.decode().strip()
-
-
-def _is_git_repo(dir: str) -> bool:
-    # TODO: This seems hacky
-    return os.path.isdir(dir + "/.git")
-
-
-def _get_git_repos(dir: str) -> List[str]:
+def _get_git_repos(dir: str) -> list[Repository]:
     """
     Recursively searches for git repos starting in (and including) the given directory.
-
-    Returns the full path of each git repo.
     """
+    # TODO: Why???
+    if "Special Characters" in dir:
+        return []
     if not os.path.isdir(dir):
         return []
-    if _is_git_repo(dir):
-        return [dir]
+    repo_path = pygit2.discover_repository(dir)
+    if repo_path:
+        return [Repository(repo_path)]
     return _flatten([_get_git_repos(os.path.join(dir, subdir)) for subdir in os.listdir(dir)])
 
 
-def _check_changes(full_path: str) -> bool:
-    return bool(
-        _invoke_git(
-            ["ls-files", "--others", "--deleted", "--modified", "--unmerged", "--exclude-standard"],
-            full_path
-        )
-    )
+def _get_short_name(repo: Repository) -> str:
+    name = repo.path[len(os.getcwd())+1:-6]
+    if not name:
+        name = "."
+    return name
 
 
-def _get_branch(full_path: str) -> str:
-    branch_name = _invoke_git(["branch", "--show-current"], full_path)
-    if branch_name:
-        return branch_name
+def _get_status(repo: Repository, name: str) -> RepoStatus:
+    branch_has_upstream = False
+    (local_ahead, local_behind) = (0, 0)
+    if repo.head_is_unborn:
+        # TODO: What to do here? Use repo.head.name?
+        branch_name = "(no commits)"
+    elif repo.head_is_detached:
+        branch_name = f"({repo.head.target.hex[:6]})"  # type: ignore
     else:
-        commit_hash = _invoke_git(["rev-parse", "--short", "HEAD"], full_path)
-        return f"({commit_hash})"
-
-
-def _get_remote_branch(full_path: str) -> str:
-    ref = _invoke_git(["symbolic-ref", "--quiet", "HEAD"], full_path)
-    return _invoke_git(["for-each-ref", f"{ref}", "--format", "%(upstream:short)"], full_path)
-
-
-def _count_local_commits(full_path: str, remote_branch: str) -> Union[int, None]:
-    output = _invoke_git(["rev-list", "--count", f"{remote_branch}..HEAD"], full_path)
-    return int(output) if output else None
-
-
-def _count_remote_commits(full_path: str, remote_branch: str) -> Union[int, None]:
-    output = _invoke_git(["rev-list", "--count", f"HEAD..{remote_branch}"], full_path)
-    return int(output) if output else None
-
-
-def _get_status(full_path: str, name: str) -> RepoStatus:
-    branch_name = _get_branch(full_path)
-    has_changes = _check_changes(full_path)
-    remote_branch_name = _get_remote_branch(full_path)
-    if remote_branch_name:
-        has_remote = True
-        local_commits = _count_local_commits(full_path, remote_branch_name)
-        remote_commits = _count_remote_commits(full_path, remote_branch_name)
-    else:
-        has_remote = False
-        local_commits = None
-        remote_commits = None
-    return RepoStatus(full_path, name, branch_name, has_changes, has_remote, local_commits, remote_commits)
+        branch_name = repo.head.shorthand
+        local_branch = repo.lookup_branch(branch_name)
+        upstream_branch = local_branch.upstream
+        if upstream_branch:
+            branch_has_upstream = True
+            (local_ahead, local_behind) = repo.ahead_behind(local_branch.target, upstream_branch.target) # type: ignore
+    is_local = not len(repo.remotes) > 0
+    # TODO: Test this
+    has_changes = len(repo.status()) > 0
+    return RepoStatus(name, branch_name, is_local, has_changes, branch_has_upstream, local_ahead, local_behind)
 
 
 def main():
     cwd = os.getcwd()
-    repo_paths = _get_git_repos(cwd)
-    repo_names = [(x, x[len(cwd)+1:]) for x in repo_paths]
-    max_name_width = max([len(name) for (_, name) in repo_names])
-    for (full_path, name) in repo_names:
-        status = _get_status(full_path, name)
-        print(status.to_string(max_name_width))
+    repos = _get_git_repos(cwd)
+    statuses = [_get_status(r, _get_short_name(r)) for r in repos]
+    name_width = max([len(s._name) for s in statuses])
+    head_width = max([len(s._branch) for s in statuses])
+    [print(s.to_string(name_width, head_width)) for s in statuses]
 
 
 if __name__ == "__main__":
