dc00597126f3770d9e3ed27e5f2bc303a03d8d29
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-11-29 11:00:06 -0500

    Move tree command to base test class

--- test/base_test_case.py
+++ test/base_test_case.py
@@ -1,7 +1,14 @@
+import subprocess
 import unittest
 
 
 class BaseTestCase(unittest.TestCase):
+    def _display_tree(self) -> None:
+        result = subprocess.run(["tree", "/f", "/a"], capture_output=True, shell=True)
+        result.check_returncode()
+        print(result.stdout.decode(encoding="utf-8"))
+
     def __init__(self, methodName: str):
-        self.maxDiff = 0
         super(BaseTestCase, self).__init__(methodName)
+        self.maxDiff = 0
+        self._display_tree()
