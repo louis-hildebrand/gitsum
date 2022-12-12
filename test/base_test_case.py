4923177a686d47d97b4aeb6adecb753cc150e1f7
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-12-11 22:43:56 -0500

    Don't allow blank lines in output

--- test/base_test_case.py
+++ test/base_test_case.py
@@ -4,10 +4,6 @@ import test.common as common
 
 
 class BaseTestCase(unittest.TestCase):
-    # Ignore blank lines in the output (for some reason there are a lot of blank lines in GitHub Actions)
-    # TODO: Is this really necessary?
-    ALLOW_BLANK_LINES = True
-
     def __init__(self, methodName: str):
         super(BaseTestCase, self).__init__(methodName)
         self.maxDiff = 0
@@ -17,9 +13,6 @@ class BaseTestCase(unittest.TestCase):
 
         result_lines = [line.rstrip() for line in actual.splitlines()]
         expected_lines = [line.rstrip() for line in expected.splitlines()]
-
-        if self.ALLOW_BLANK_LINES:
-            result_lines = [x for x in result_lines if x]
 
         # Check number of lines
         self.assertEqual(len(expected_lines), len(result_lines), diff)
