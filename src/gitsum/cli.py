e4e022c618015e78a72c11e052ec78e54c73967f
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2023-02-03 23:39:18 -0500

    Add --version flag

--- src/gitsum/cli.py
+++ src/gitsum/cli.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 from gitsum.status import RepoStatus
+import gitsum
 import gitsum.search as search
 
 
@@ -16,19 +17,25 @@ def _parse_args() -> argparse.Namespace:
     parser.add_argument("-f", "--fetch", action="store_true", help="fetch before getting status")
     parser.add_argument("-o", "--outside-files", action="store_true", help="list files and directories that are not inside a Git repository")
     parser.add_argument("-O", "--only-outside-files", action="store_true", help="list files and directories that are not inside a Git repository and exit")
+    parser.add_argument("-V", "--version", action="store_true", help="display the version number and exit")
 
     return parser.parse_args()
 
 
-def _show_git_summary(fetch: bool, list_outside_files: bool, only_outside_files: bool) -> None:
+def _show_git_summary(args: argparse.Namespace) -> None:
+    if args.version:
+        gitsum_version = gitsum.__version__
+        print(f"gitsum v{gitsum_version}")
+        sys.exit(0)
+
     cwd = Path(os.getcwd())
-    repos = search.find_git_repos(cwd, list_outside_files or only_outside_files)
-    if only_outside_files:
+    repos = search.find_git_repos(cwd, args.outside_files or args.only_outside_files)
+    if args.only_outside_files:
         return
 
     num_repos = len(repos)
     print(f"Found {num_repos} Git {'repository' if num_repos == 1 else 'repositories'}.")
-    statuses = [RepoStatus(r, fetch) for r in repos]
+    statuses = [RepoStatus(r, args.fetch) for r in repos]
     if statuses:
         name_width = max([len(s.name) for s in statuses])
         head_width = max([len(s.head) for s in statuses])
@@ -39,8 +46,7 @@ def _show_git_summary(fetch: bool, list_outside_files: bool, only_outside_files:
 def main() -> None:
     try:
         args = _parse_args()
-
-        _show_git_summary(args.fetch, args.outside_files, args.only_outside_files)
+        _show_git_summary(args)
     except KeyboardInterrupt:
         print("Cancelled.")
         sys.exit(1)
